8813699772c2c9dd461a6fbd1fba52572eb15aa7
Chris Underwoodgit config user.email cunderwoodmn@gmail.comgit statusgit init;cd <christopherunderwood@Christophers-MacBook-Pro.local>
2015-09-11 14:36:13 -0700

    added code for first spec that did not pass

--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,7 @@
-describe("", function() {
-  // it('returns "Hello Universe" for a string entered with "Universe" replaced with "World"', function() {
-  //   expect(wordFindReplace("Hello Universe", "Universe", "World")).to.equal("Hello World");
-  // });
+describe("wordFindReplace", function() {
+  it('returns "Hello Universe" for a string entered with "Universe" replaced with "World"', function() {
+    expect(wordFindReplace("Hello Universe", "Universe", "World")).to.equal("Hello World");
+  });
 
 
 });
