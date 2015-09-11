7b866c919eb8b2589c40266024fd1898a0f4be50
Chris Underwoodgit config user.email cunderwoodmn@gmail.comgit statusgit init;cd <christopherunderwood@Christophers-MacBook-Pro.local>
2015-09-11 15:42:18 -0700

    replace non-working array with simpler string.replace method. First spec passes and much dryer code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,22 @@
 var wordFindReplace = function(sentence, searchWord, replaceWord) {
-	var sentenceWord = sentence;
-	var sentenceArrary = sentenceWord.split(" ");
+	// var sentence = sentence;
+	// var sentenceArray = sentence.split(" ");
  	var searchWord = searchWord;
  	var replaceWord = replaceWord;
-	var result = ""
+	var result = sentence.replace(searchWord, replaceWord);
 
- 	sentenceArrary.forEach(function (itemWord){
- 		if (itemWord == searchWord) {
-			itemWord = replaceWord;
-		}
+	return result;
+ // 	sentenceArray.forEach(function (itemWord, i){
+ // 		if (searchWord == itemWord) {
+	//
+	// 		sentenceArray = replaceWord;
+	// 	}
+	//
+	// });
+	//
+	// return result = sentenceArray.join(" ");
 
-	});
-
-	return result = sentenceArrary.join(" ")
-
-}
+};
 
 $(document).ready(function() {
   $("form#wordFindReplace").submit(function(event) {
@@ -34,10 +36,10 @@ $(document).ready(function() {
 	var colors = ['lightred', 'red', 'darkred'];
 
 
-	setInterval(function() {
-		var backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-		$(document.body).css('background-color', backgroundColor);
-
-	}, 10000);
+	// setInterval(function() {
+	// 	var backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+	// 	$(document.body).css('background-color', backgroundColor);
+	//
+	// }, 10000);
 
 });
