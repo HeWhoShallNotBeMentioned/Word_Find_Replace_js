633cb3f19d132258b1d5301d270e3b6f4737f0d9
Chris Underwoodgit config user.email cunderwoodmn@gmail.comgit statusgit init;cd <christopherunderwood@Christophers-MacBook-Pro.local>
2015-09-11 16:12:02 -0700

    add successful test for multi word string replacement and an updated README

--- spec/specs.js
+++ spec/specs.js
@@ -2,6 +2,8 @@ describe("wordFindReplace", function() {
   it('returns "Hello Universe" for a string entered with "Universe" replaced with "World"', function() {
     expect(wordFindReplace("Hello Universe", "Universe", "World")).to.equal("Hello World");
   });
-
+  it('returns "Chris is a smelly man" for a string entered with "silly boy" replaced with "smelly man"', function() {
+    expect(wordFindReplace("Chris is a silly boy", "silly boy", "smelly man")).to.equal("Chris is a smelly man");
+  });
 
 });
